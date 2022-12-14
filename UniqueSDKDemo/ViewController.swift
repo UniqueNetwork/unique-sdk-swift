102674bd44b19fe0c74208b9e7f87ac887526d78
MaxOl <some@mail.com>
2022-12-14 14:24:33 +0300

    change to opal

--- UniqueSDKDemo/ViewController.swift
+++ UniqueSDKDemo/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Unique.setConfiguration(.unique)
+        Unique.setConfiguration(.opal)
         Unique.savePasscode("1234")
     }
     
